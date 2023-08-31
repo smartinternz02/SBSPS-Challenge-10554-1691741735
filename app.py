68a0cf58222ca0a57404eec39540deb9b95fac84
NITIN YADAV <102907637+MRKUMARCOOL@users.noreply.github.com>
2023-08-31 21:54:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template
+import pickle
 
+
+model = pickle.load(popPred.pkl,'rb')
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,8 +15,9 @@ def process_year():
     
     # Do something with the year data
     result = f"You entered the year: {year}"
-    
-    return result
+    print(result)
+    r2 = model.predict([[year]])
+    return render_template('ml.html', o1=result, o2 = r2)
 
 if __name__ == '__main__':
     app.run(debug=True)
